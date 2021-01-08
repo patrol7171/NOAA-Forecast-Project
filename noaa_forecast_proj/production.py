daf32b6a14191bdcdd9dd56112b9e64f2c5811ea
Patricia Rollins <patrol7171@gmail.com>
2021-01-08 09:23:15 -0500

    Updated settings & production files
    updated db and Debug settings

--- noaa_forecast_proj/production.py
+++ noaa_forecast_proj/production.py
@@ -22,15 +22,15 @@ MIDDLEWARE = [
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
-# Configure Postgres database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'] + "@" + hostname,
-        'PASSWORD': os.environ['DBPASS'] 
-    }
-}
+# hostname = os.environ['DBHOST']
+# # Configure Postgres database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ['DBNAME'],
+#         'HOST': hostname + ".postgres.database.azure.com",
+#         'USER': os.environ['DBUSER'] + "@" + hostname,
+#         'PASSWORD': os.environ['DBPASS'] 
+#     }
+# }
 
--- noaa_forecast_proj/settings.py
+++ noaa_forecast_proj/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = []
 
-DEBUG = False
+DEBUG = True
 GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 IPSTACK_API_KEY = os.environ['IPSTACK_API_KEY']
 
