639d58bc30bcc877593f30d78a877d976bdbda8b
Patricia Rollins <patrol7171@gmail.com>
2021-01-08 11:05:47 -0500

    Update production.py
    Removed middleware info

--- noaa_forecast_proj/production.py
+++ noaa_forecast_proj/production.py
@@ -7,16 +7,16 @@ import os
 #ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # WhiteNoise configuration
-MIDDLEWARE = [                                                                   
-    'django.middleware.security.SecurityMiddleware',                            
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',                      
-    'django.middleware.common.CommonMiddleware',                                 
-    'django.middleware.csrf.CsrfViewMiddleware',                                 
-    'django.contrib.auth.middleware.AuthenticationMiddleware',                   
-    'django.contrib.messages.middleware.MessageMiddleware',                      
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
-]
+# MIDDLEWARE = [                                                                   
+#     'django.middleware.security.SecurityMiddleware',                            
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',                      
+#     'django.middleware.common.CommonMiddleware',                                 
+#     'django.middleware.csrf.CsrfViewMiddleware',                                 
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',                   
+#     'django.contrib.messages.middleware.MessageMiddleware',                      
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+# ]
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
